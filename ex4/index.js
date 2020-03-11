bdcf91acd5ac22cdf605ad1ee878c67e646872ca
PEDRO LUCAS SOARES ZANOTI <pedro.lszanoti@senacsp.edu.br>
2020-03-11 16:49:27 -0300

    servidor

--- ex4/index.js
+++ ex4/index.js
@@ -8,6 +8,10 @@
 let express = require('express')
 let app = express()
 let port = 3000
+//rota
+app.get('/', (req, res) => {
+    res.send('<h1>Home</h1>')
+})
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
   console.log('Para derrubar o servidor: ctrl + c');
